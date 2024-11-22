dca7ba85fefa0b108ecf6c7fcb6ba5ae7639a95e
Brandon Shea <brandon.shea1994@icloud.com>
2024-11-22 00:20:16 -0330

    triangle and EquilateralTriangle as ET needed no changes

--- src/Problem4/EquilateralTriangle.java
+++ src/Problem4/EquilateralTriangle.java
@@ -1,4 +1,13 @@
 package Problem4;
 
-public class EquilateralTriangle {
+public class EquilateralTriangle extends Triangle {
+
+    public EquilateralTriangle(String name, double side) {
+        super(name, side, side, side);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Equilateral)";
+    }
 }
