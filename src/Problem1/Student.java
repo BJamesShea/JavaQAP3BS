f32e8f93227d2d4b335de1e67dade8c8431f41a3
Brandon Shea <brandon.shea1994@icloud.com>
2024-11-21 21:37:19 -0330

    Student class

--- src/Problem1/Student.java
+++ src/Problem1/Student.java
@@ -1,4 +1,37 @@
 package Problem1;
 
-public class Student {
+public class Student extends Person {
+    private String myIdNum;
+    private double myGPA;
+
+
+    public Student(String myName, int myAge, String myGender, String myIdNum, double myGPA) {
+        super(myName, myAge, myGender);
+        this.myIdNum = myIdNum;
+        this.myGPA = myGPA;
+    }
+
+
+    public String getMyIdNum() {
+        return myIdNum;
+    }
+
+    public double getMyGPA() {
+        return myGPA;
+    }
+
+
+    public void setMyIdNum(String myIdNum) {
+        this.myIdNum = myIdNum;
+    }
+
+    public void setMyGPA(double myGPA) {
+        this.myGPA = myGPA;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() + ", ID: " + myIdNum + ", GPA: " + myGPA;
+    }
 }
