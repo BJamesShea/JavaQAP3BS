55e4b2d7b826ce98303caf3f8f62113f7347c650
Brandon Shea <brandon.shea1994@icloud.com>
2024-11-21 23:36:18 -0330

    reusing shape code but implementing scalable

--- src/Problem4/Shape.java
+++ src/Problem4/Shape.java
@@ -1,4 +1,21 @@
 package Problem4;
 
-public class Shape {
+public abstract class Shape implements Scalable {
+    protected String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double computeArea();
+    public abstract double computePerimeter();
+
+    @Override
+    public String toString() {
+        return "Shape: " + name;
+    }
 }
