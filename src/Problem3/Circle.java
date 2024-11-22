3c9741d0b7533878eff9bc4b6f09adf1783878f6
Brandon Shea <brandon.shea1994@icloud.com>
2024-11-21 22:38:17 -0330

    circle class

--- src/Problem3/Circle.java
+++ src/Problem3/Circle.java
@@ -1,4 +1,36 @@
 package Problem3;
 
-public class Circle {
+public class Circle extends Shape {
+    private double radius;
+
+
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+
+    @Override
+    public double computeArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double computePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Radius: " + radius + ", Area: " + computeArea() + ", Perimeter: " + computePerimeter();
+    }
 }
